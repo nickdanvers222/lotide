3f5371e8bc1b4be00808a8fb6c8cc2e1532ff980
Nick <ndanvers222@gmail.com>
2020-01-08 22:19:14 +0000

    Function to determine the key of a desired value

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`😍😍😍Assertion Passed : ${actual} = ${expected}`);
+    } else if (actual !== expected) {
+      console.log(`😡😡😡Assertion Failed: ${actual} !== ${actual}`);
+    }
+  
+  };
+////////
+ const bestTVShowsByGenre = { 
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+
+const findKeyByValue = function(obj, value) {
+    keys = Object.keys(obj);
+    for(const genres of keys) {
+        if(obj[genres] === value) {
+            return genres;
+        }
+    }
+  }
+
+////////
+
+
+
+
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
