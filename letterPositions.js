14fa80538bc366024a985673b221559f95b7f158
Nick <ndanvers222@gmail.com>
2020-01-08 21:48:44 +0000

    a function to determine the index position of each letter in a sentence

---
+++ letterPositions.js
@@ -0,0 +1,19 @@
+const letterPositions = function(sentence) {
+    const results = {};
+    let newSentence = sentence.toLowerCase();
+    let position = 0;
+//
+    for(const places of newSentence) {
+        if(places === " ") {
+            position += 1;
+        continue;
+        } else if(results[places]) {
+            results[places].push(newSentence.indexOf(places, position));
+        } else {
+            results[places] = [newSentence.indexOf(places)];
+        }
+        position += 1;
+    }
+    return results;
+  };
+console.log(letterPositions("lighthouse in the house"));
