d1ee7eca4be4e56bd02847d2df0e581bb47f7bd9
Nick <ndanvers222@gmail.com>
2020-01-10 02:00:22 +0000

    updating name of file

---
+++ forSorting.js
@@ -0,0 +1,20 @@
+const students = [
+    { id: 1, name: "bruce",    age: 40 },
+    { id: 2, name: "zoidberg", age: 22 },
+    { id: 3, name: "alex",     age: 22 },
+    { id: 4, name: "alex",     age: 30 }
+  ];
+  console.log(students.sort(function(a, b) {
+      if(a.name === b.name) {
+          return b.age - a.age;
+      } else {
+    var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+  }
+}));
