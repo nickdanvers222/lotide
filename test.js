04431d58f21434baeffb0b875ff061665601e52d
Nick <ndanvers222@gmail.com>
2020-01-10 01:52:11 +0000

    a function

--- test.js
+++ test.js
@@ -1,18 +1,20 @@
-const input = [
-    { x: 3, y: 4 },
-    { x: 12, y: 5 },
-    { x: 8, y: 15 }
+const students = [
+    { id: 1, name: "bruce",    age: 40 },
+    { id: 2, name: "zoidberg", age: 22 },
+    { id: 3, name: "alex",     age: 22 },
+    { id: 4, name: "alex",     age: 30 }
   ];
-
-  const result = input.map(function(item){
-       return (Math.sqrt(item["x"] **2 + item["y"] **2 ));
-
+  console.log(students.sort(function(a, b) {
+      if(a.name === b.name) {
+          return b.age - a.age;
+      } else {
+    var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
     }
-);   
-  
- console.log(result[0] === 5);
- console.log(result[1] === 13);
- console.log(result[2] === 17);
-
-
-console.log(result);
+    if (nameA > nameB) {
+      return 1;
+    }
+  }
+}));
