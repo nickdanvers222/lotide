11ec2daba0bf180219c2aa4586886d436ce3462b
Nick <ndanvers222@gmail.com>
2020-01-13 06:56:45 +0000

    added mocha and chai to tailTest & middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,14 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 
 
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
+//assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
+
+describe("middle", () => {
+    it("should be 3,4 given [1,2,3,4,5,6]", () => {
+        assert.deepEqual(middle([1,2,3,4,5,6]), [3,4]);
+    })
+    it("should be 3 given [1,2,3,4,5", () => {
+        assert.equal(middle([1,2,3,4,5]), 3);
+    })
+})
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,9 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-//
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("tail", () => {
+    it("Should return [2,3,4,5] given [1,2,3,4,5]", () => {
+        assert.deepEqual(tail([1,2,3,4,5]), [2,3,4,5])
+    })
+})
