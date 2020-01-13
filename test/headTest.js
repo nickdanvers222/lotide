1746e6a3a5fe27424cb366f164ae8da00a1509f0
Nick <ndanvers222@gmail.com>
2020-01-13 06:41:21 +0000

    included mocha and chai into the directory, refactored the headTest.js testfile to include it.

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,11 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["first", "second"]), "first");
-
+describe("head", function() {
+    it("Should return the 5 for [5]", function() {
+        assert.strictEqual(head([5,4,2]), 5);
+    });
+    it("should return 1 for [1,3,5]", () => {
+        assert.strictEqual(head([1,3,5]), 1);
+    })
+}) 
